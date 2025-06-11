b94377744bcf791ac874e84176cb3aff63f1ae4b
Dams51 <90524688+Dams51@users.noreply.github.com>
2025-06-12 00:22:16 +0200

    Cleaning unused constants

--- custom_components/pleinchamp/const.py
+++ custom_components/pleinchamp/const.py
@@ -22,35 +22,16 @@ CONF_LOCATION_NAME = "location_name"
 CONF_LATITUDE = "latitude"
 CONF_LONGITUDE = "longitude"
 CONF_TIMEZONE_INFO = "timezone_info"
-CONF_CONDITION_CLOUDCOVER_WEIGHT = "cloudcover_weight"
-CONF_CONDITION_CLOUDCOVER_HIGH_WEAKENING = "cloudcover_high_weakening"
-CONF_CONDITION_CLOUDCOVER_MEDIUM_WEAKENING = "cloudcover_medium_weakening"
-CONF_CONDITION_CLOUDCOVER_LOW_WEAKENING = "cloudcover_low_weakening"
-CONF_CONDITION_FOG_WEIGHT = "fog_weight"
-CONF_CONDITION_SEEING_WEIGHT = "seeing_weight"
-CONF_CONDITION_TRANSPARENCY_WEIGHT = "transparency_weight"
-CONF_CONDITION_CALM_WEIGHT = "calm_weight"
-CONF_EXPERIMENTAL_FEATURES = "experimental_features"
 
 # #####################################################
 # Default values
 # #####################################################
-DEFAULT_ATTRIBUTION = "Powered by Pleinchamp"
-EXPERIMENTAL_ATTRIBUTION = "Powered by Pleinchamp"
+DEFAULT_ATTRIBUTION = "Data provided by Pleinchamp"
 DEFAULT_FORECAST_INTERVAL = 5
 FORECAST_INTERVAL_MIN = 1
 FORECAST_INTERVAL_MAX = 240
 DEFAULT_LOCATION_NAME = "Backyard"
 DEFAULT_TIMEZONE_INFO = "Etc/UTC"
-DEFAULT_CONDITION_CLOUDCOVER_WEIGHT = 3
-DEFAULT_CONDITION_CLOUDCOVER_HIGH_WEAKENING = 100
-DEFAULT_CONDITION_CLOUDCOVER_MEDIUM_WEAKENING = 100
-DEFAULT_CONDITION_CLOUDCOVER_LOW_WEAKENING = 100
-DEFAULT_CONDITION_FOG_WEIGHT = 3
-DEFAULT_CONDITION_SEEING_WEIGHT = 2
-DEFAULT_CONDITION_TRANSPARENCY_WEIGHT = 1
-DEFAULT_CONDITION_CALM_WEIGHT = 2
-DEFAULT_EXPERIMENTAL_FEATURES = False
 DEFAULT_FORECAST_TYPE = "daily"
 
 DEFAULT_CACHE_TIMEOUT = 1770
@@ -58,70 +39,8 @@ DEFAULT_TIMEOUT = 10
 BASE_URL_PLEINCHAMP = "https://api.prod.pleinchamp.com/"
 
 # #####################################################
-# Attribute values
-# #####################################################
-ATTR_LOCATION_NAME = "location_name"
-ATTR_WEATHER_TIME_SHIFT = "time_shift"
-ATTR_WEATHER_CLOUDCOVER = "cloudcover_percentage"
-ATTR_WEATHER_CLOUDLESS = "cloudless_percentage"
-ATTR_WEATHER_CLOUD_AREA_FRACTION = "cloud_area_fraction"
-ATTR_WEATHER_CLOUD_AREA_FRACTION_HIGH = "cloud_area_fraction_high"
-ATTR_WEATHER_CLOUD_AREA_FRACTION_MEDIUM = "cloud_area_fraction_medium"
-ATTR_WEATHER_CLOUD_AREA_FRACTION_LOW = "cloud_area_fraction_low"
-ATTR_WEATHER_FOG_AREA_FRACTION = "fog_area_fraction"
-ATTR_WEATHER_FOG2M_AREA_FRACTION = "fog2m_area_fraction"
-ATTR_WEATHER_DEWPOINT = "dewpoint"
-ATTR_WEATHER_SEEING = "seeing"
-ATTR_WEATHER_SEEING_PERCENTAGE = "seeing_percentage"
-ATTR_WEATHER_TRANSPARENCY = "transparency"
-ATTR_WEATHER_TRANSPARENCY_PERCENTAGE = "transparency_percentage"
-ATTR_WEATHER_LIFTED_INDEX = "lifted_index"
-ATTR_WEATHER_WIND = "calm_percentage"
-ATTR_WEATHER_CONDITION = "condition_percentage"
-ATTR_WEATHER_CONDITION_PLAIN = "condition_plain"
-ATTR_WEATHER_PRECIPITATION_AMOUNT = "precipitation_amount"
-ATTR_WEATHER_DEEPSKY_TODAY_DAYNAME = "deepsky_forecast_today_dayname"
-ATTR_WEATHER_DEEPSKY_TODAY_PLAIN = "deepsky_forecast_today_plain"
-ATTR_WEATHER_DEEPSKY_TODAY_PRECIP6 = "deepsky_forecast_today_precipitation_amount6"
-ATTR_WEATHER_DEEPSKY_TODAY_DESC = "deepsky_forecast_today_desc"
-ATTR_WEATHER_DEEPSKY_TOMORROW_DAYNAME = "deepsky_forecast_tomorrow_dayname"
-ATTR_WEATHER_DEEPSKY_TOMORROW_PLAIN = "deepsky_forecast_tomorrow_plain"
-ATTR_WEATHER_DEEPSKY_TOMORROW_PRECIP6 = "deepsky_forecast_tomorrow_precipitation_amount6"
-ATTR_WEATHER_DEEPSKY_TOMORROW_DESC = "deepsky_forecast_tomorrow_desc"
-ATTR_WEATHER_SUN_NEXT_RISING = "sun_next_rising"
-ATTR_WEATHER_SUN_NEXT_SETTING = "sun_next_setting"
-ATTR_WEATHER_SUN_NEXT_RISING_NAUTICAL = "sun_next_rising_nautical"
-ATTR_WEATHER_SUN_NEXT_SETTING_NAUTICAL = "sun_next_setting_nautical"
-ATTR_WEATHER_SUN_NEXT_RISING_ASTRO = "sun_next_rising_astro"
-ATTR_WEATHER_SUN_NEXT_SETTING_ASTRO = "sun_next_setting_astro"
-ATTR_WEATHER_MOON_NEXT_RISING = "moon_next_rising"
-ATTR_WEATHER_MOON_NEXT_SETTING = "moon_next_setting"
-ATTR_WEATHER_MOON_PHASE = "moon_phase"
-ATTR_WEATHER_MOON_NEXT_NEW_MOON = "moon_next_new_moon"
-ATTR_WEATHER_MOON_NEXT_FULL_MOON = "moon_next_full_moon"
-ATTR_WEATHER_DEEP_SKY_DARKNESS = "deep_sky_darkness"
-ATTR_WEATHER_ASTRONOMICAL_DARKNESS = "night_duration_astronomical"
-ATTR_FORECAST_CLOUDCOVER = "cloudcover_percentage"
-ATTR_FORECAST_CLOUDLESS = "cloudless_percentage"
-ATTR_FORECAST_CLOUD_AREA_FRACTION = "cloud_area_fraction"
-ATTR_FORECAST_CLOUD_AREA_FRACTION_HIGH = "cloud_area_fraction_high"
-ATTR_FORECAST_CLOUD_AREA_FRACTION_MEDIUM = "cloud_area_fraction_medium"
-ATTR_FORECAST_CLOUD_AREA_FRACTION_LOW = "cloud_area_fraction_low"
-ATTR_FORECAST_FOG_AREA_FRACTION = "fog_area_fraction"
-ATTR_FORECAST_FOG2M_AREA_FRACTION = "fog2m_area_fraction"
-ATTR_FORECAST_SEEING = "seeing"
-ATTR_FORECAST_SEEING_PERCENTAGE = "seeing_percentage"
-ATTR_FORECAST_TRANSPARENCY = "transparency"
-ATTR_FORECAST_TRANSPARENCY_PERCENTAGE = "transparency_percentage"
-ATTR_FORECAST_CALM = "calm_percentage"
-ATTR_FORECAST_LIFTED_INDEX = "lifted_index"
-ATTR_FORECAST_HUMIDITY = "humidity"
-ATTR_FORECAST_PRECIPITATION_AMOUNT = "precipitation_amount"
-
-# #####################################################
 # Lists
 # #####################################################
-CONDITION_CLASSES = ["excellent", "good", "fair", "poor", "bad"]
 
 TIMEZONES = [
     "Africa/Abidjan",
